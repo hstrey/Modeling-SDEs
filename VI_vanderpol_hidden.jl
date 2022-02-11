3b155789440ab66da8e675e7041ee2b8ba0c016d
Helmut Strey <Helmut.Strey@gmail.com>
2022-02-11 12:36:47 -0500

    vdp sde EM

--- VI_vanderpol_hidden.jl
+++ VI_vanderpol_hidden.jl
@@ -2,6 +2,7 @@ using DifferentialEquations
 using DifferentialEquations.EnsembleAnalysis
 using Turing
 using Distributions
+using FillArrays
 #using MCMCChains
 using StatsPlots
 using Random
@@ -12,19 +13,19 @@ using Plots
 Random.seed!(08)
 
 function vanderpol(du,u,p,t)
-  x1,x2 = u
-  θ,ϕ = p
+    x1,x2 = u
+    θ,ϕ = p
 
-  du[1] = x2
-  du[2] = θ*(1-x1^2)*x2 - x1
+    du[1] = x2
+    du[2] = θ*(1-x1^2)*x2 - x1
 end
 
 function add_noise(du,u,p,t)
-  x1,x2 = u
-  θ,ϕ = p
-  
-  du[1] = ϕ #   0	
-  du[2] = ϕ
+    x1,x2 = u
+    θ,ϕ = p
+
+    du[1] = ϕ #   0	
+    du[2] = ϕ
 end
 
 u0 = [0.1, 0.1]
@@ -35,6 +36,7 @@ p = [1.0,0.1]
 prob1 = SDEProblem(vanderpol, add_noise, u0, tspan, p)
 sol = solve(prob1,SOSRI(),saveat=0.1)
 time = sol.t
+dt=0.1
 
 p1 = plot(sol)
 
@@ -45,6 +47,10 @@ data = solve(ensembleprob,SOSRI(),EnsembleThreads(),saveat=0.1,trajectories=50)
 #plot(EnsembleSummary(data))
 #ar=Array(sol)
 ar=Array(data) # first index x1,x2; second index time; third index trajectory
+ns = rand(Normal(0,.1),2,size(ar,2),size(ar,3)) #this is the measurement noise
+
+#simulation + noise
+arn = ar + ns
 
 slope = 5.0
 scale = 50.0
@@ -62,105 +68,76 @@ x2 = ar[2,:,:]
 x1_sig = g_sigmoid(ar[1,:,:],slope,scale)
 x2_sig = g_sigmoid(ar[2,:,:],slope,scale)
 
-p = plot(x1)
-plot!(x2)
+# x1 and x2 are the untransformed solutions
+x1n = arn[1,:,:]
+x2n = arn[2,:,:]
 
-ns = rand(Normal(0,1),2,size(ar,2),size(ar,3)) #this is the measurement noise
+# x1_sig and x2_sig are the transformed solutions
+x1n_sig = g_sigmoid(arn[1,:,:],slope,scale)
+x2n_sig = g_sigmoid(arn[2,:,:],slope,scale)
 
-  Turing.setadbackend(:forwarddiff)
+p = plot(x1n)
+plot(x2n)
 
-  
-  @model function fitvp(data,prob1)
-    #σ ~ InverseGamma(8,3)
-    σ = 1
-    #θ ~ Uniform(0,10)
-    #θ ~ Normal(0,2)
-    θ ~ Gamma(2,5)
-    #θ ~ TruncatedNormal(0,1,0,Inf)
-    ϕ ~ Uniform(0,0.2)
+Turing.setadbackend(:forwarddiff)
+
+@model function fitvp(data,prob1)
+    σ = 0.1
+    θ ~ TruncatedNormal(0,3,0,Inf)
+    ϕ ~ Uniform(0,0.5)
     #ϕ ~ Gamma(1.1,0.1)
-    x01 ~ Normal(0.1,0.1)
-    
-    x02 ~ Normal(0.1,0.1)
+    #x01 ~ Normal(0.1,0.1)
+    #x02 ~ Normal(0.1,0.1)
 
     p = [θ,ϕ]
-    u0 = [x01,x02]
+    u0 = [0.1,0.1]
     #u0 = typeof(ϕ).(prob1.u0)
     #u0 = u0
 
-  if θ >=0
-
     prob = remake(prob1,u0=u0,p=p)
     predicted = solve(prob,SOSRI(),saveat=0.1)#,maxiters=1e7)
-    
+
     if predicted.retcode != :Success
-        
-        Turing.acclogp!(__varinfo__, -Inf)
+        Turing.@addlogprob! -Inf
+        return
     end
-
-    x_pred = Array(predicted)
-    y_pred = g_sigmoid.(x_pred,slope,scale)
-
-    
-    for j = 1:size(data,3)
-    for i = 1:min(length(predicted),size(data,2))#size(data,2)  
-    #for i = 1:length(data[:,1])
-     # data[i,1] ~ Normal(y_pred[1,i],σ)
-     # data[i,2] ~ Normal(y_pred[2,i],σ)
-
-     data[1,i,j] ~ Normal(y_pred[1,i],σ)
-     data[2,i,j] ~ Normal(y_pred[2,i],σ)
+    for i in 1:size(data,2)
+        data[:,i] ~ MvNormal(sol[1,:],σ)
     end
-    end
-    
-  else
-    Turing.acclogp!(__varinfo__, -Inf)
-  end
-
-  end
-
-  #model = fitvp(y,prob1)
-  
-  model = fitvp(ysim+ns,prob1)
-  
-  # model = fitvp(sol,prob1)
-
-  #chain = sample(model, NUTS(0.25), MCMCThreads(),1000, 1)#,init_theta = [0.1, 0.5, 0.1])
-   chain = sample(model, NUTS(0.65),2000)
-
-  plot(chain)
-  cr=Array(chain)
-
-  pl2= plot(sol,alpha=2,legend=false)
-
-  
-  for k in 1:300
-      resol = solve(remake(prob1,p=cr[rand(1:1000),1:2]),SOSRI(),saveat=0.1)
-      plot!(pl2,resol, alpha=0.1, color = "#BBBBBB", legend = false)
-  end
-
-  pl2
-  
-  Random.seed!(91)
-
-  advi = ADVI(30, 1000)
-  
-  q = vi(model, advi);
-
-  samples=rand(q,10000)
-  mn=mean(samples,dims=2)
-  density(samples[1,:],legend=false,xlims=(0,5))
-
-  pl= plot(sol,alpha=2,legend=false)
-  for k in 1:300
-    #resol = solve(remake(prob1,u0=samples[3:4,rand(1:10000)],p=samples[1:2,rand(1:10000)]),SOSRI(),saveat=0.1)
-    resol = solve(remake(prob1,p=samples[1:2,rand(1:10000)]),SOSRI(),saveat=0.1)
-    
-    plot!(pl,resol, alpha=0.1, color = "#BBBBBB", legend = false,ylims=(-5,5))
-  end
-pl
-open("samples4.txt","w") do io
-	writedlm(io, samples)
 end
 
-samples = readdlm("samples4.txt", '\t', Float64,'\n')
+@model function fitvpEM(datax, datay)
+    σ = 0.1
+    θ ~ TruncatedNormal(0,3,0,Inf)
+    ϕ ~ Uniform(0,0.5)
+    for i in 2:length(datax)
+        dx1 = datay[i-1]
+        dx2 = θ*(1-datax[i-1]^2)*datay[i-1] - datax[i-1]
+        datax[i] ~ Normal(datax[i-1]+dx1*dt,ϕ*sqrt(dt))
+        datay[i] ~ Normal(datay[i-1]+dx2*dt,ϕ*sqrt(dt))
+    end
+end
+
+@model function fitvpEMn(dataxn, datayn)
+    σ = 0.1
+    θ ~ TruncatedNormal(0,3,0,Inf)
+    ϕ ~ Uniform(0,0.5)
+    for i in 2:length(datax)
+        dx1 = datay[i-1]
+        dx2 = θ*(1-datax[i-1]^2)*datay[i-1] - datax[i-1]
+        xh ~ Normal(datax[i-1]+dx1*dt,ϕ*sqrt(dt))
+        yh ~ Normal(datay[i-1]+dx2*dt,ϕ*sqrt(dt))
+        data
+    end
+end
+
+#model = fitvp(y,prob1)
+
+model = fitvpEM(sol[1,:],sol[2,:])
+
+# model = fitvp(sol,prob1)
+
+#chain = sample(model, NUTS(0.25), MCMCThreads(),1000, 1)#,init_theta = [0.1, 0.5, 0.1])
+chain = sample(model, NUTS(0.65),2000)
+
+plot(chain)
