0485d00e89e100109901a5302d3ab345ab0b4254
Helmut Strey <Helmut.Strey@gmail.com>
2022-03-04 12:29:51 -0500

    hidden variable VI is working now

--- VI_vanderpol_hidden.jl
+++ VI_vanderpol_hidden.jl
@@ -3,12 +3,11 @@ using DifferentialEquations.EnsembleAnalysis
 using Turing
 using Distributions
 using FillArrays
-#using MCMCChains
 using StatsPlots
 using Random
 using Plots
+using ReverseDiff
 using Turing: Variational
-#using DelimitedFiles
 
 Random.seed!(08)
 
@@ -32,18 +31,18 @@ u0 = [0.1, 0.1]
 tspan = [0.0, 20]
 p = [1.0,0.1]
 
-
-prob1 = SDEProblem(vanderpol, add_noise, u0, tspan, p)
-sol = solve(prob1,SOSRI(),saveat=0.1)
-time = sol.t
 dt=0.1
+prob1 = SDEProblem(vanderpol, add_noise, u0, tspan, p)
+sol = solve(prob1,EM(),dt=dt)
+time = sol.t
+
 
 p1 = plot(sol)
 
 #plot(sol, vars = (1,2))
 
 ensembleprob = EnsembleProblem(prob1)
-data = solve(ensembleprob,SOSRI(),EnsembleThreads(),saveat=0.1,trajectories=50)
+data = solve(ensembleprob,EM(),EnsembleThreads(),dt=dt,trajectories=50)
 #plot(EnsembleSummary(data))
 #ar=Array(sol)
 ar=Array(data) # first index x1,x2; second index time; third index trajectory
@@ -73,41 +72,16 @@ x1n = arn[1,:,:]
 x2n = arn[2,:,:]
 
 # x1_sig and x2_sig are the transformed solutions
-x1n_sig = g_sigmoid(arn[1,:,:],slope,scale)
-x2n_sig = g_sigmoid(arn[2,:,:],slope,scale)
+x1n_sig = g_sigmoid(ar[1,:,:],slope,scale) + ns[1,:,:]
+x2n_sig = g_sigmoid(ar[2,:,:],slope,scale) + ns[2,:,:]
 
-p = plot(x1n[:,1:5])
-plot(x2n)
+plot(x1n_sig[:,1])
+plot!(x2n_sig[:,1])
 
-Turing.setadbackend(:forwarddiff)
-
-@model function fitvp(data,prob1)
-    σ = 0.1
-    θ ~ TruncatedNormal(0,3,0,Inf)
-    ϕ ~ Uniform(0,0.5)
-    #ϕ ~ Gamma(1.1,0.1)
-    #x01 ~ Normal(0.1,0.1)
-    #x02 ~ Normal(0.1,0.1)
-
-    p = [θ,ϕ]
-    u0 = [0.1,0.1]
-    #u0 = typeof(ϕ).(prob1.u0)
-    #u0 = u0
-
-    prob = remake(prob1,u0=u0,p=p)
-    predicted = solve(prob,SOSRI(),saveat=0.1)#,maxiters=1e7)
-
-    if predicted.retcode != :Success
-        Turing.@addlogprob! -Inf
-        return
-    end
-    for i in 1:size(data,2)
-        data[:,i] ~ MvNormal(sol[1,:],σ)
-    end
-end
+Turing.setadbackend(:reversediff)
 
 @model function fitvpEM(datax, datay)
-    θ ~ TruncatedNormal(0,3,0,Inf)
+    θ ~ Gamma(0.1,10.0)
     ϕ ~ Uniform(0,0.5)
     dxh = datay[1:end-1]
     dyh = θ .* (1 .- datax[1:end-1] .^ 2) .* datay[1:end-1] .- datax[1:end-1]
@@ -115,6 +89,7 @@ end
     datay[2:end] ~ MvNormal(datay[1:end-1] .+ dyh * dt,ϕ*sqrt(dt))
 end
 
+# here I am trying to fit and SDE with f(du,u,p,t) and g(du,u,p,t)
 @model function fitSDE(f,g,dt,datax,datay)
     θ ~ TruncatedNormal(0,3,0,Inf)
     ϕ ~ Uniform(0,0.5)
@@ -131,37 +106,69 @@ end
 
 @model function fitvpEMn(datax, datay)
     σ = 0.1
-    θ ~ TruncatedNormal(0,3,0,Inf)
-    ϕ ~ Uniform(0,0.5)
-    dxh = datay[1:end-1]
-    dyh = θ .* (1 .- datax[1:end-1] .^ 2) .* datay[1:end-1] .- datax[1:end-1]
-    xh ~ MvNormal(datax[1:end-1] .+ dxh * dt,ϕ*sqrt(dt))
-    yh ~ MvNormal(datay[1:end-1] .+ dyh * dt,ϕ*sqrt(dt))
-    datax[2:end] ~ MvNormal(xh,σ)
-    datay[2:end] ~ MvNormal(yh,σ)
+    θ ~ Gamma(0.25,4.0)
+    ϕ ~ Uniform(0,0.2)
+    # create arrays for hidden variables
+    xh = zero(datax)
+    yh = zero(datay)
+    xh[1] = 0.1
+    yh[1] = 0.1
+    for i in 2:length(datax)
+        dxh = yh[i-1]
+        dyh = θ * (1.0 - xh[i-1]^2) * yh[i-1] - xh[i-1]
+        xh[i] ~ Normal(xh[i-1] + dxh * dt,ϕ*sqrt(dt))
+        yh[i] ~ Normal(yh[i-1] + dyh * dt,ϕ*sqrt(dt))
+    end
+    datax ~ MvNormal(xh,σ)
+    datay ~ MvNormal(yh,σ)
+end
+
+@model function fitvpEMnsig(datax, datay)
+    σ = 0.1
+    θ ~ Gamma(0.1,10.0)
+    ϕ ~ Uniform(0,0.2)
+        # create arrays for hidden variables
+        xh = zero(datax)
+        yh = zero(datay)
+        xh[1] = 0.1
+        yh[1] = 0.1
+        for i in 2:length(datax)
+            dxh = yh[i-1]
+            dyh = θ * (1.0 - xh[i-1]^2) * yh[i-1] - xh[i-1]
+            xh[i] ~ Normal(xh[i-1] + dxh * dt,ϕ*sqrt(dt))
+            yh[i] ~ Normal(yh[i-1] + dyh * dt,ϕ*sqrt(dt))
+        end
+        datax ~ MvNormal(g_sigmoid(xh,slope,scale),σ)
+        datay ~ MvNormal(g_sigmoid(xh,slope,scale),σ)
 end
 
 #model = fitvp(y,prob1)
 
 model = fitvpEMn(x1n[:,1],x2n[:,1])
-
+model = fitvpEMnsig(x1n_sig[:,1],x2n_sig[:,1])
 plot(x1n[:,1])
 plot!(x2n[:,1])
 # model = fitvp(sol,prob1)
 
 #chain = sample(model, NUTS(0.25), MCMCThreads(),1000, 1)#,init_theta = [0.1, 0.5, 0.1])
-chain = sample(model, NUTS(0.65),1000)
+chain = sample(model, NUTS(0.65),1000,init_theta = [1.0,0.1])
 chainarray = Array(chain)
-chainx = chainarray[:,3:203]
-chainy = chainarray[:,204:end]
+chainx = chainarray[:,3:202]
+chainy = chainarray[:,203:end]
 chainx_avg = mean(chainx,dims=1)[:]
 chainy_avg = mean(chainy,dims=1)[:]
 chainx_std = std(chainx,dims=1)[:]
 chainy_std = std(chainy,dims=1)[:]
 
-plot(0:0.1:20,x1n[:,1],label="x data",xlabel="time in sec",ylabel="amplitude",linewidth=2)
-plot!(0:0.1:20,x2n[:,1],label="y data",linewidth=2)
+scatter(0:0.1:20,x1n[:,1],label="x data",xlabel="time in sec",ylabel="amplitude")
+scatter!(0:0.1:20,x2n[:,1],label="y data")
+plot!(0:0.1:20,x1[:,1],linewidth=2,ribbon=0.3,label="x")
+plot!(0:0.1:20,x2[:,1],linewidth=2,ribbon=0.3,label="y")
 savefig("VPdata.png")
+
+plot(0:0.1:20,x1n_sig[:,1],label="x sig data",xlabel="time in sec",ylabel="amplitude",linewidth=2)
+plot!(0:0.1:20,x2n_sig[:,1],label="y sig data",linewidth=2)
+savefig("VPdatasig.png")
 
 plot(0.1:0.1:20,chainx_avg,ribbon=chainx_std,label="x pred",xlabel="time in sec",ylabel="amplitude")
 plot!(0.1:0.1:20,chainy_avg,ribbon=chainy_std,label="y pred")
@@ -177,9 +184,14 @@ advi = ADVI(10, 1000)
 q = vi(model, advi)
 
 # sampling
-z = rand(q, 10_000)
+z = rand(q, 1000)
 avg = vec(mean(z; dims = 2))
-chainx_advi = avg[3:503]
-chainy_advi = avg[504:end]
-plot(chainx_advi)
-plot!(chainy_advi)
+zstd = vec(std(z; dims = 2))
+chainx_advi_mean = avg[3:202]
+chainy_advi_mean = avg[203:end]
+chainx_advi_std = zstd[3:202]
+chainy_advi_std = zstd[203:end]
+
+plot(0.1:0.1:20,chainx_advi_mean,ribbon=chainx_advi_std,label="x pred",xlabel="time in sec",ylabel="amplitude")
+plot!(0.1:0.1:20,chainy_advi_mean,ribbon=chainy_advi_std,label="y pred")
+savefig("VPpred.png")
