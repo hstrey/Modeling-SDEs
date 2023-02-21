a963c2ebf9f9c2271e9468680a56263a0990cace
Helmut Strey <Helmut.Strey@stonybrook.edu>
2023-02-21 08:18:51 -0500

    improved fit and figure

--- modelingchi2.jl
+++ modelingchi2.jl
@@ -16,34 +16,35 @@ scatter(l1,l2,marker_z=log.(z))
 ll12 = hcat(l1, l2)
 
 function twoD_Gaussian(xy, p)
-    amplitude, xo, yo, sigma_x, sigma_y, rho = p
+    offset, amplitude, xo, yo, sigma_x, sigma_y, rho = p
 
     # creating linear meshgrid from xy
     x = xy[:, 1]
     y = xy[:, 2]
-    g = amplitude .* (((x .- xo).^2)/sigma_x^2 - 2 .* rho .* (x .- xo)/sigma_x .* (y .- yo)/sigma_y + ((y .- yo).^2)/sigma_y^2)
+    g = offset .+ amplitude .* (((x .- xo).^2)/sigma_x^2 - 2 .* rho .* (x .- xo)/sigma_x .* (y .- yo)/sigma_y + ((y .- yo).^2)/sigma_y^2)
     return g[:]
 end
 
 function twoDG(x,y,p)
-    amplitude, xo, yo, sigma_x, sigma_y, rho = p
+    offset, amplitude, xo, yo, sigma_x, sigma_y, rho = p
 
     # creating linear meshgrid from xy
-    return amplitude * (((x - xo)^2)/sigma_x^2 - 2 * rho * (x - xo)/sigma_x * (y - yo)/sigma_y + ((y - yo)^2)/sigma_y^2)
+    return offset + amplitude * (((x - xo)^2)/sigma_x^2 - 2 * rho * (x - xo)/sigma_x * (y - yo)/sigma_y + ((y - yo)^2)/sigma_y^2)
 end
 
-p0 = Float64.([3, 0.5, 0.18, 0.01, 0.01, -0.6])
+p0 = Float64.([-10.0, 3, 0.5, 0.18, 0.01, 0.01, -0.6])
 
 fit = LsqFit.curve_fit(twoD_Gaussian, ll12, z, p0)
 confidence_inter = confidence_interval(fit,0.05)
 p = coef(fit)
-twoDGp(x,y) = exp(-twoDG(x,y,p))
+twoDGp(x,y) = twoDG(x,y,p)
 
 xr = -0.2:0.01:0.5
 yr = -0.2:0.01:0.5
 
-contour(xr,yr,twoDGp,xlabel="L1",ylabel="L2")
+contour(xr,yr,twoD,xlabel="L1",ylabel="L2")
 savefig("twoDGauss.png")
 
-contour(xr,yr,twoDGp,xlabel="L1",ylabel="L2",linewidth=3,label=nothing)
+contour(xr,yr,twoDGp,xlabel="L1",ylabel="L2",zlim=(-10,2.5),clim=(-10,2.5),levels=[0.15,.5],linewidth=3,label=nothing)
 scatter!(l1,l2,marker_z=log.(z),xlim=(-0.3,0.6),ylim=(-0.3,0.6),label=nothing)
+savefig("twoDGauss.png")
