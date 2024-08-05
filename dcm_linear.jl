7a4bc85fd36d69c206f8846602207cf8bcd25d33
Helmut Strey <Helmut.Strey@stonybrook.edu>
2024-08-05 12:16:55 -0400

    sampling is now working

--- dcm_linear.jl
+++ dcm_linear.jl
@@ -7,6 +7,9 @@ using LinearAlgebra
 using Distributions
 using StatsPlots
 using DSP
+using ReverseDiff
+using ADTypes
+using OptimizationOptimJL: NelderMead
 
 # function f = spm_Gpdf(x,h,l)
 #     % Probability Density Function (PDF) of Gamma distribution
@@ -39,21 +42,25 @@ using DSP
 #     hrf = hrf([0:floor(p(7)/RT)]*fMRI_T + 1);
 #     hrf = hrf'/sum(hrf);
 
-https://github.com/neurodebian/spm12/blob/master/spm_hrf.m
+# https://github.com/neurodebian/spm12/blob/master/spm_hrf.m
 function HRF(x,rtu=6.0,δ1=6.0,δ2=16.0,τ1=1.0,τ2=1.0,C=0.0)
     return pdf.(Gamma(δ1,τ1),x)-pdf.(Gamma(δ2,τ2),x)/rtu + C
 end
 
-t_hrf = 0:0.1:600
+function myconv(x,y)
+    [ sum(y .* circshift(x, i)) for i in 0:length(x)-1]
+end
+
+t_hrf = 0:0.1:60
 hrf = HRF.(t_hrf)
 
 plot(t_hrf, hrf)
 
 # Parameters that I'd imagine are reasonable
-sim_len = 600.0
+sim_len = 60.0
 dt = 0.1
 nd = Normal(0,1)
-dd = -Gamma(5,1) + 1
+dd = -Gamma(5,1)
 # Define some variables
 @parameters w11 w12 w22 w21 γ
 @variables x1(t) x2(t)
@@ -82,13 +89,13 @@ sol = solve(prob, EM(), dt=dt)
 
 plot(sol)
 
-fmrix1 = conv(hrf,sol[1,:])[1:length(sol[1,:])]
-fmrix2 = conv(hrf,sol[2,:])[1:length(sol[2,:])]
+fmrix1 = myconv(sol[1,:],hrf)
+fmrix2 = myconv(sol[2,:],hrf)
 
-plot(sol.t,sol[1,:],xlim=(0,100))
-plot!(sol.t,fmrix1,xlim=(0,100))
+plot(sol.t,sol[2,:])
+plot!(sol.t,fmrix2)
 
-@model function fitlinear(x1,x2)
+@model function fitlinear(x1,x2,dt)
     m11 ~ -Gamma(5,1) + 1
     m22 ~ -Gamma(5,1) + 1
     m12 ~ Normal(0,1)
@@ -102,5 +109,35 @@ plot!(sol.t,fmrix1,xlim=(0,100))
     end
 end
 
-model = fitlinear(sol[1,:],sol[2,:])
+@model function fitlinearhrf(x1,x2,dt)
+    m11 ~ -Gamma(5,1) + 1
+    m22 ~ -Gamma(5,1) + 1
+    m12 ~ Normal(0,1)
+    m21 ~ Normal(0,1)
+
+    # latent x1 and x2
+    lx1 = tzeros(length(x1))
+    lx2 = tzeros(length(x2))
+
+    lx1[1] = 0.0 # initial condition (could be a parameter later)
+    lx2[1] = 0.0
+
+    for i in 2:length(x1)
+        dx1 = m11 * lx1[i-1] + m12 * lx2[i-1]
+        dx2 = m22 * lx2[i-1] + m21 * lx1[i-1]
+        lx1[i] ~ Normal(lx1[i-1] + dx1 * dt,sqrt(dt))
+        lx2[i] ~ Normal(lx2[i-1] + dx2 * dt,sqrt(dt))
+    end
+    x1 ~ MvNormal(myconv(hrf,lx1)[1:length(x1)],0.5) # observed width of Gaussian
+    x2 ~ MvNormal(myconv(hrf,lx2)[1:length(x2)],0.5) # may be a parameter also
+end
+
+model = fitlinear(sol[1,:],sol[2,:],0.1)
 chain = sample(model, NUTS(), 1000)
+
+modelhrf = fitlinearhrf(sol[1,:],sol[2,:],0.1)
+
+# we first get a MAP estimate to start the chain in a viable spot
+map_estimate = maximum_a_posteriori(modelhrf, NelderMead(); adtype=AutoReverseDiff())
+chainhrf = sample(modelhrf, NUTS(0.65, adtype=AutoReverseDiff()), 1000; initial_params=map_estimate.values.array, save_state=true)
+chn2 = sample(modelhrf, NUTS(0.65, adtype=AutoReverseDiff()), 3000; resume_from=chainhrf)
